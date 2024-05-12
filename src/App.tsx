1abb3f3d44a094c237bc52c4204274493f08d3da
oyazigi <mambo0001@outlook.com>
2024-05-11 22:29:16 -0300

    concluindo projeto

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,24 @@ import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import ListaCategoria from './components/categoria/listaCategoria/ListaCategoria';
+import FormularioCategoria from './components/categoria/formularioCategoria/FormCategoria';
+import DeletarCategoria from './components/categoria/deletarCategoria/DeletarCategoria';
+
 function App() {
   return (
     <>
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
-            <Home />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategoria />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
           </div>
           <Footer />
         </BrowserRouter>
